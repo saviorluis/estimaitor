31da60afb9978a1502feef28b898dc499cbee634
saviorluis <velascoluis07@gmail.com>
2025-04-01 13:20:12 -0400

    Fix PDF generation by adding missing Roboto font variants

--- src/components/QuotePDF.tsx
+++ src/components/QuotePDF.tsx
@@ -9,6 +9,8 @@ Font.register({
   fonts: [
     { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-regular-webfont.ttf', fontWeight: 'normal' },
     { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-bold-webfont.ttf', fontWeight: 'bold' },
+    { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-italic-webfont.ttf', fontWeight: 'normal', fontStyle: 'italic' },
+    { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-bolditalic-webfont.ttf', fontWeight: 'bold', fontStyle: 'italic' },
   ],
 });
 
